45c3b906917f8e4c4e838d518df68e02986b5169
abhaysp95 <abhaysp95@protonmail.com>
2024-08-27 22:21:28 +0530

    feat: added split method for the "bad list" (first way)
      - returns the new list from the next node of the node which matched
        the argument

--- src/first.rs
+++ src/first.rs
@@ -40,12 +40,20 @@ impl List {
         }
     }
     
-    pub fn merge(&mut self, list: List) {
-        match self.head {
-            Link::Empty => {},
-            Link::More(_) => {},
+    /// Split on the basis of element match
+    /// Returns the new list from the next node of the node which matched the elem provided as
+    /// argument
+    pub fn split(&mut self, elem: i32) -> Option<List> {
+        let mut current = &mut self.head;
+        while let Link::More(ref mut node) = current {
+            if node.elem == elem {
+                let mut list = List::new();
+                list.head = mem::replace(&mut node.next, Link::Empty);
+                return Some(list);
+            }
+            current = &mut node.next;
         }
-
+        None
     }
 }
 
@@ -91,5 +99,43 @@ mod test {
         // check exhaustion
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_split() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+        list.push(5);
+
+        let no_list = list.split(10);
+        assert!(no_list.is_none());
+
+        let list2 = list.split(1);
+        assert!(list2.is_some());  // we got the list
+        // but it shouldn't have any element
+        let mut list2 = list2.unwrap(); 
+        assert_eq!(list2.pop(), None);
+
+        // move after first element
+        let moved_list = list.split(5);
+        assert!(moved_list.is_some());
+        // old list should have one element now
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), None);
+
+        // break from between
+        let mut moved_list = moved_list.unwrap();
+        let mut half_list = moved_list.split(3);
+        assert!(half_list.is_some());
+        // exhaust both the list now
+        assert_eq!(moved_list.pop(), Some(4));
+        assert_eq!(moved_list.pop(), Some(3));
+        assert_eq!(moved_list.pop(), None);
+        assert_eq!(half_list.as_mut().unwrap().pop(), Some(2));
+        assert_eq!(half_list.as_mut().unwrap().pop(), Some(1));
+        assert_eq!(half_list.as_mut().unwrap().pop(), None);
     }
 }

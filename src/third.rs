cfbdefb252bc10b7775b810e7478ccd9cac2ef8b
abhaysp95 <abhaysp95@protonmail.com>
2024-09-01 23:28:20 +0530

    feat: test for prepend and tail for third list

--- src/third.rs
+++ src/third.rs
@@ -64,6 +64,32 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn test_prepend_tail() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // Make sure empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+}
+
+
 // NOTE: we'll come back to these
 //
 // pub fn prepend(&mut self, elem: T) {

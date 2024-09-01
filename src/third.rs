3bf646387a2d33bcb4ae7958512a58b9687febf1
abhaysp95 <abhaysp95@protonmail.com>
2024-09-01 23:00:18 +0530

    feat: prepend and tail method for third list

--- src/third.rs
+++ src/third.rs
@@ -1,29 +1,42 @@
 use std::rc::Rc;
 
 pub struct List<T> {
-    head: Link<T>
+    head: Link<T>,
 }
 
 pub type Link<T> = Option<Rc<Node<T>>>;
 
 pub struct Node<T> {
     elem: T,
-    next: Link<T>
+    next: Link<T>,
 }
 
-impl<T> List<T> where T: PartialEq {
+impl<T> List<T>
+where
+    T: PartialEq,
+{
     pub fn new() -> List<T> {
+        Self { head: None }
+    }
+
+    pub fn prepend(&self, elem: T) -> List<T> {
         Self {
-            head: None
+            head: Some(Rc::new(Node {
+                elem,
+                next: self.head.clone(),
+            })),
         }
     }
 
+    pub fn tail(&self) -> List<T> {
+        Self {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
 }
 
-
-
 // NOTE: we'll come back to these
-// 
+//
 // pub fn prepend(&mut self, elem: T) {
 //     self.head.take().map(|node| {
 //         self.head = Some(Rc::new(Node {

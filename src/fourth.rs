5684e6424b56c20802a4ac9e41f4cb876ec3b333
abhaysp95 <abhaysp95@protonmail.com>
2024-09-08 18:36:44 +0530

    feat: using RefCell for interior mutability
      - introduced push_front

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
 pub struct Node<T> {
     elem: T,
@@ -6,7 +6,7 @@ pub struct Node<T> {
     next: Link<T>
 }
 
-pub type Link<T> = Option<Rc<Node<T>>>;
+pub type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 pub struct List<T> {
     head: Link<T>,
@@ -14,20 +14,38 @@ pub struct List<T> {
 }
 
 impl<T> Node<T> {
-    fn new(elem: T) -> Self {
-        Self {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+
+        Rc::new(RefCell::new(Self {
             elem,
             prev: None,
             next: None,
-        }
+        }))
     }
 }
 
 impl<T> List<T> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             head: None,
             tail: None,
+        }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        let new_node = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // NOTE: if it just would've been Rc, you wouldn't have been able to mutate it for
+                // old_head.prev or for new_node.next
+                old_head.borrow_mut().prev = Some(new_node.clone());
+                new_node.borrow_mut().next = Some(old_head);
+                self.head = Some(new_node);
+            },
+            None => {
+                self.tail = Some(new_node.clone());
+                self.head = Some(new_node);
+            }
         }
     }
 }

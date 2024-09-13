7cf341e7e2e08590e41ef546560c6b62bb8bf681
abhaysp95 <abhaysp95@protonmail.com>
2024-09-13 23:31:53 +0530

    feat: added popfront
      - use ok() to turn Result to Option
      - use into_inner to consume RefCell

--- src/fourth.rs
+++ src/fourth.rs
@@ -3,19 +3,18 @@ use std::{cell::RefCell, rc::Rc};
 pub struct Node<T> {
     elem: T,
     prev: Link<T>,
-    next: Link<T>
+    next: Link<T>,
 }
 
 pub type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 pub struct List<T> {
     head: Link<T>,
-    tail: Link<T>
+    tail: Link<T>,
 }
 
 impl<T> Node<T> {
     fn new(elem: T) -> Rc<RefCell<Self>> {
-
         Rc::new(RefCell::new(Self {
             elem,
             prev: None,
@@ -41,11 +40,59 @@ impl<T> List<T> {
                 old_head.borrow_mut().prev = Some(new_node.clone());
                 new_node.borrow_mut().next = Some(old_head);
                 self.head = Some(new_node);
-            },
+            }
             None => {
                 self.tail = Some(new_node.clone());
                 self.head = Some(new_node);
             }
         }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        if let Some(old_head) = self.head.take() {
+            if let Some(new_head) = old_head.borrow_mut().next.take() {
+                new_head.borrow_mut().prev.take();
+                self.head = Some(new_head);
+            } else {
+                self.tail.take();
+            }
+            Some(Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem)
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop_front(), None);
+
+        // Populate list
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push_front(4);
+        list.push_front(5);
+
+        // Check normal removal
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
     }
 }
